b8eed39fec724f5bd5a37636a469816472255b5e
Michael Teter <m@teter.is>
2023-02-04 10:53:27 +0700

    feat: add context (global state) service

--- lib/product.rb
+++ lib/product.rb
@@ -1,7 +1,7 @@
 class Product
   attr_reader :sku, :name, :price
 
-  def initialize args
+  def initialize(args)
     args.each { |k,v| instance_variable_set("@#{k}", v) unless v.nil? }
   end
 end
---
+++ test/context_service_test.rb
@@ -0,0 +1,21 @@
+require "minitest/autorun"
+require_relative "../lib/context_service.rb"
+
+class TestContextService < Minitest::Test
+  def test_get_context
+    ctx = ContextService.get_context
+    assert ctx != nil
+  end
+
+  def test_update_context
+    original_ctx = ContextService.get_context
+    foo = { 'a key' => 'a value' }
+    returned_context = ContextService.update_context!(foo: foo)
+    new_ctx = ContextService.get_context
+
+    assert original_ctx != new_ctx
+
+    assert_equal returned_context, new_ctx
+    assert_equal new_ctx[:foo], foo
+  end
+end
