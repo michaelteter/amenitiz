ef7b3936b1de411857fb6fb9f4142f035dc6b43d
Michael Teter <m@teter.is>
2023-02-06 18:35:21 +0700

    feat: first discount rule implemented

--- lib/discount_service.rb
+++ lib/discount_service.rb
@@ -29,4 +29,36 @@ module DiscountService
 
     discounts
   end
+
+  def apply_buy_n_get_m_free(discount:, product:, qty:)
+    n = discount.rule_params['buy_count']
+    m = discount.rule_params['free_count']
+
+    # TODO: get another pair of eyes to review this; there is probably a
+    #   simpler formula that doesn't require a conditional
+    pay_qty = if qty <= (n+m)
+      [qty, n].min
+    else
+      full_discount_sets = (qty / (n+m))
+      full_discount_qty = full_discount_sets * (n+m)
+      remaining_qty = (qty - full_discount_qty) % n
+      full_discount_sets * n + remaining_qty
+    end
+
+    pay_qty * product.price
+  end
+
+  def apply_rule(discount:, product:, qty:)
+    fn = 'apply_' + discount.rule_name.downcase
+    return unless self.method_defined?(fn)
+
+    self.send(fn, discount: discount, product: product, qty: qty)
+  end
+
+  def discounted_line_total(product:, qty:, discounts: nil)
+    discount = (discounts || load_discounts[product.sku])&.values&.first
+    return if discount.nil? # Let the caller calculate the normal total
+
+    apply_rule(discount: discount, product: product, qty: qty)
+  end
 end
--- test/discount_service_test.rb
+++ test/discount_service_test.rb
@@ -1,9 +1,20 @@
 require "minitest/autorun"
+require_relative "../lib/cart"
+require_relative "../lib/catalog_service"
 require_relative "../lib/discount_service"
 
 class TestDiscountService < Minitest::Test
   def setup
+    @catalog = CatalogService.load_products
     @discounts = DiscountService.load_discounts
+    @sample_cart = Cart.new('GR1' => 2, 'SR1' => 5, 'CF1' => 10)
+
+    @product = Product.new(sku: 'FOO', name: 'Foo d', price: 1)
+    @discount = Discount.new(code:        'FOO-N-for-M',
+                             product_sku: 'FOO',
+                             description: 'Buy {{buy_count}}, and get {{free_count}} more free!',
+                             rule_name:   'BUY_N_GET_M_FREE',
+                             rule_params: { 'buy_count' => 5, 'free_count' => 2 })
   end
 
   def test_load_discounts
@@ -11,5 +22,60 @@ class TestDiscountService < Minitest::Test
     @discounts.each do |_code, discount|
       assert discount.code.is_a?(String)
     end
+  end
+
+  def test_apply_buy_n_get_m_free
+    total = DiscountService.apply_buy_n_get_m_free(discount: @discount, product: @product, qty: 0)
+    assert_equal total, 0
+
+    total = DiscountService.apply_buy_n_get_m_free(discount: @discount, product: @product, qty: 1)
+    assert_equal total, 1 * @product.price
+
+    total = DiscountService.apply_buy_n_get_m_free(discount: @discount, product: @product, qty: 5)
+    assert_equal total, 5 * @product.price
+
+    total = DiscountService.apply_buy_n_get_m_free(discount: @discount, product: @product, qty: 6)
+    assert_equal total, 5 * @product.price
+
+    total = DiscountService.apply_buy_n_get_m_free(discount: @discount, product: @product, qty: 7)
+    assert_equal total, 5 * @product.price
+
+    total = DiscountService.apply_buy_n_get_m_free(discount: @discount, product: @product, qty: 8)
+    assert_equal total, 6 * @product.price
+
+    total = DiscountService.apply_buy_n_get_m_free(discount: @discount, product: @product, qty: 16)
+    assert_equal total, 12 * @product.price
+  end
+
+  def test_apply_rule
+    total = DiscountService.apply_rule(discount: @discount, product: @product, qty: 16)
+    assert_equal total, 12 * @product.price
+
+    # Test against non-existent rule
+    discount = Discount.new(code:        'BOGUS',
+                            product_sku: 'FOO',
+                            description: '...',
+                            rule_name:   'NOT_IMPLEMENTED_YET',
+                            rule_params: { 'buy_count' => 5, 'free_count' => 2 })
+    total = DiscountService.apply_rule(discount: discount, product: @product, qty: 16)
+    assert_nil total
+  end
+
+  def test_discounted_line_total
+    discounts = { @discount.code => @discount }
+
+    total = DiscountService.discounted_line_total(product: @product, qty: 16, discounts: discounts)
+    assert_equal total, 12 * @product.price
+
+    # Simulate not finding a matching discount for this product.
+    # Since there's no discount that applies, we get nil back.
+    total = DiscountService.discounted_line_total(product: @product, qty: 16, discounts: {})
+    assert_nil total
+
+    # In pursuit of coverage... let DiscountService load the discounts; but our test
+    #   product 'FOO' refers to a test discount code which does not exist in the default
+    #   data.
+    total = DiscountService.discounted_line_total(product: @product, qty: 16)
+    assert_nil total
   end
 end
